0dc1f9c7235b9b550ea0d9abd5592824b8edba49
badakis <badakis@uth.gr>
2019-04-16 12:15:04 +0300

    Test and Test and Set for locks

--- src/lock.c
+++ src/lock.c
@@ -1,21 +1,21 @@
 #include "lock.h"
 
 void lock_init(lock_t *lock) {
-	*lock = 0;
+    *lock = 0;
 }
 
 void lock_acquire(lock_t *lock) {
-	while (!__sync_bool_compare_and_swap(lock, 0, 1)) {
-    	sched_yield();
-    	// while(*p){
-    	// 	_mm_pause();
-    	// }
-    }
+    int res;
+
+    do {
+        while (*lock) {
+            _mm_pause();
+        } 
+        res = __sync_bool_compare_and_swap(lock, 0, 1);
+    } while (!res);
+    
 }
 
 void lock_release(lock_t *lock) {
-	asm volatile (
-		"movl $0, %0;" 
-    	:"=r"(*lock)
-    );
+    *lock = 0;
 }
